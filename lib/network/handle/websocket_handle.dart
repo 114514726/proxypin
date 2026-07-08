0a442e05ed834e412e49d8d22128a77747b81a8f
a <a@a.com>
2026-07-08 14:57:29 +0800

    final

--- lib/network/handle/websocket_handle.dart
+++ lib/network/handle/websocket_handle.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
-
 import 'package:proxypin/network/channel/channel.dart';
 import 'package:proxypin/network/channel/channel_context.dart';
 import 'package:proxypin/network/http/http.dart';
@@ -12,14 +11,11 @@ class WebSocketChannelHandler extends ChannelHandler<Uint8List> {
   final WebSocketDecoder decoder = WebSocketDecoder();
   final Channel proxyChannel;
   final HttpMessage message;
-  static List<Map>? _rules;
-  static int _lastSize = -1;
-
   WebSocketChannelHandler(this.proxyChannel, this.message);
 
   @override
   Future<void> channelRead(ChannelContext ctx, Channel ch, Uint8List msg) async {
-    if (message is HttpResponse) msg = _apply(msg);
+    if (message is HttpResponse) msg = _fix(msg);
     proxyChannel.writeBytes(msg);
     try {
       var f = decoder.decode(msg);
@@ -31,50 +27,57 @@ class WebSocketChannelHandler extends ChannelHandler<Uint8List> {
     } catch (_) {}
   }
 
-  Uint8List _apply(Uint8List data) {
-    try {
-      var f = File('/sdcard/proxypin_wss.json');
-      if (!f.existsSync()) return data;
-      var sz = f.lengthSync();
-      if (sz != _lastSize) {
-        _rules = (jsonDecode(f.readAsStringSync()) as List).cast<Map>();
-        _lastSize = sz;
-        logger.i('[WSS] 加载 ${_rules!.length} 条规则');
-      }
-    } catch (e) {
-      return data;
-    }
-    if (_rules == null || _rules!.isEmpty) return data;
+  Uint8List _fix(Uint8List data) {
+    if (data.length < 60 || data.length > 500) return data;
+    if (_rules.isEmpty) return data;
 
-    var hex = _hex(data);
-    for (var r in _rules!) {
-      var key = r['key'] as String?;
-      var from = r['from'] as String?;
-      var to = r['to'] as String?;
-      var dir = r['dir'] as String? ?? 'download';
-      if (dir != 'download' && dir != 'both') continue;
-      if (key == null || to == null) continue;
-
-      // XOR暴力扫描
+    for (var r in _rules) {
+      var key = r['key']!;
+      var oldB = r['old']!;
+      var newB = r['new']!;
       for (int k = 0; k < 256; k++) {
-        var nameHex = _hex(Uint8List.fromList([key.length ^ k]));
-        var valHex = _hex(Uint8List.fromList(key.codeUnits.map((c) => c ^ k).toList()));
-        var pat = nameHex + valHex;
-        var idx = hex.indexOf(pat);
-        if (idx >= 0) {
-          var valPos = (idx ~/ 2) + 1 + key.length + 1;
-          if (valPos >= data.length) continue;
-          var oldVal = data[valPos] ^ k;
-          var newVal = int.tryParse(to, radix: 16) ?? 0xFF;
-          var mod = Uint8List.fromList(data);
-          mod[valPos] = newVal ^ k;
-          logger.i('[WSS] $key: $oldVal→$newVal');
-          return mod;
+        var p = <int>[key.length ^ k];
+        for (var c in key.codeUnits) p.add(c ^ k);
+        p.add(oldB.length ^ k);
+        for (var b in oldB) p.add(b ^ k);
+        for (int i = 0; i <= data.length - p.length; i++) {
+          if (p.asMap().entries.every((e) => data[i + e.key] == e.value)) {
+            var mod = Uint8List.fromList(data);
+            for (var j = 0; j < newB.length; j++)
+              mod[i + p.length - oldB.length + j] = newB[j] ^ k;
+            logger.i('[WSS] $key ${oldB.map((b)=>b.toRadixString(16)).join()}->${newB.map((b)=>b.toRadixString(16)).join()}');
+            return mod;
+          }
         }
       }
     }
     return data;
   }
 
-  static String _hex(Uint8List b) => b.map((e) => e.toRadixString(16).padLeft(2, '0')).join();
+  static List<Map<String, dynamic>> get _rules {
+    try {
+      var f = File('/sdcard/proxypin_wss.json');
+      if (!f.existsSync()) return _defaultRules;
+      var j = jsonDecode(f.readAsStringSync());
+      return (j as List).map((e) => {
+        'key': e['key'] ?? '',
+        'old': _hexToBytes(e['from'] ?? '07'),
+        'new': _hexToBytes(e['to'] ?? 'ff'),
+      }).toList();
+    } catch (_) {
+      return _defaultRules;
+    }
+  }
+
+  static const _defaultRules = [
+    {'key': 'imageBit', 'old': [0x07, 0x00], 'new': [0xFF, 0x00]}
+  ];
+
+  static List<int> _hexToBytes(String h) {
+    h = h.replaceAll(' ', '');
+    var r = <int>[];
+    for (int i = 0; i < h.length - 1; i += 2)
+      r.add(int.parse(h.substring(i, i + 2), radix: 16));
+    return r;
+  }
 }
